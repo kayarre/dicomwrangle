9bf9f662c5bd85c1ce4297bae879fbb740b36b6c
Kurt Sansom <kayarre@gmail.com>
2015-06-24 23:02:59 -0700

    add new files from previous investigation
    Kurt Sansom

---
+++ dcmreader.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 17 14:01:12 2015
+
+@author: sansomk
+"""
+
+import dicom
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Slider, Button, RadioButtons
+
+
+dcmpath='/Users/sansomk/Downloads/0.4/102'
+dcm_files = []
+for dirname, subdirlist, filelist in os.walk(dcmpath):
+    for filen in  filelist:
+        #print(dirname, subdirlist, filelist)
+        #break
+        try:
+            filePath = os.path.join(dirname,filen)
+            #print(filePath)
+            f = dicom.read_file(filePath, stop_before_pixels=True)
+            dcm_files.append(filePath)
+        except:
+            print("error: {0}".format(filen))
+            
+print('hello')
+# Get ref file
+RefDs = dicom.read_file(dcm_files[0])
+
+# Load dimensions based on the number of rows, columns, and slices (along the Z axis)
+ConstPixelDims = (int(RefDs.Rows), int(RefDs.Columns), len(dcm_files))
+
+#check ti see whether the image has been interpolated
+if (hasattr(RefDs, 'SpacingBetweenSlices')):
+    if (RefDs.SpacingBetweenSlices < RefDs.SliceThickness ):
+        z_spacing = RefDs.SpacingBetweenSlices
+else:
+    z_spacing = float(RefDs.SliceThickness)
+
+# Load spacing values (in mm)
+ConstPixelSpacing = (float(RefDs.PixelSpacing[0]),
+                     float(RefDs.PixelSpacing[1]), z_spacing)
+
+x = np.arange(0.0, (ConstPixelDims[0]+1)*ConstPixelSpacing[0], ConstPixelSpacing[0])
+y = np.arange(0.0, (ConstPixelDims[1]+1)*ConstPixelSpacing[1], ConstPixelSpacing[1])
+z = np.arange(0.0, (ConstPixelDims[2]+1)*ConstPixelSpacing[2], ConstPixelSpacing[2])
+
+# The array is sized based on 'ConstPixelDims'
+ArrayDicom = np.zeros(ConstPixelDims, dtype=RefDs.pixel_array.dtype)
+print(ArrayDicom.shape)
+# loop through all the DICOM files
+for filenameDCM in dcm_files:
+    # read the file
+    ds = dicom.read_file(filenameDCM)
+    # store the raw image data
+    ArrayDicom[:, : , dcm_files.index(filenameDCM)] = ds.pixel_array
+
+testindx = np.where(ArrayDicom != 0)
+#print(np.max(testindx[0]), np.max(testindx[1]))
+#print(np.min(testindx[0]), np.min(testindx[1]))
+minx = np.min(testindx[0])
+maxx = np.max(testindx[0])
+miny = np.min(testindx[1])
+maxy = np.max(testindx[1])
+minz = np.min(testindx[2])
+maxz = np.max(testindx[2])
+
+#ConstPixelDims = (int(maxx-minx), int(maxy-miny), len(dcm_files))
+#new = RefDs.pixel_array
+#new[testindx] = -9999999
+
+fig1 = plt.figure(dpi=300)
+ax1 = plt.axes()
+ax1.set_aspect('equal', 'datalim')
+ax1.pcolormesh(x[miny:maxy], y[minx:maxx],
+               np.flipud(ArrayDicom[minx:maxx, miny:maxy, 490]), cmap='gray')
+
+'''
+for filen in dcm_files:
+    #print(filen)
+    f = dicom.read_file(filen, stop_before_pixels=True)
+    #print(f.PixelSpacing, f.PixelSpacing, f.SpacingBetweenSlices)
+    print(f.SliceLocation)
+'''
---
+++ taginvestigation.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 18 13:45:03 2015
+
+@author: sansomk
+"""
+
+import dicom
+import os
+#import numpy as np
+#import matplotlib.pyplot as plt
+#from matplotlib.widgets import Slider, Button, RadioButtons
+import fnmatch
+
+
+dcmpath='/Users/sansomk/Downloads/E431791260_FlowVol_01/'
+dcm_files = []
+count = 0
+dict_test = {}
+# tags 
+# TriggerTime = time of the image
+# SliceLocation = spatial location of slice.
+# SliceThickness = the thickness of the image
+# need to figure our how to convert images to axial ones
+slice_location = []
+trigger_time = []
+image_dict = {}
+count = 0
+fn_dict = {"X":"FlowX_*.dcm", "Y":"Flowy_*.dcm", "Z":"FlowZ_*.dcm", "MAG":"Mag_*.dcm"}
+new_dict = {}
+for dirname, subdirlist, filelist in os.walk(dcmpath):
+    for filen in  filelist:
+        try:
+            filePath = os.path.join(dirname,filen)
+            #print(filePath)
+            f = dicom.read_file(filePath, stop_before_pixels=True)
+            #print(dirname, subdirlist, filelist)
+            #print(filePath)
+            #print(f.SliceLocation)
+        except:
+            print("error: {0}".format(filen))
+            continue
+        
+        #dictionary of images
+        if (f.TriggerTime not in image_dict.keys()):
+            image_dict[f.TriggerTime] = {}
+        if (f.SliceLocation not in image_dict[f.TriggerTime].keys()):
+            image_dict[f.TriggerTime][f.SliceLocation] = {}
+        
+        for fn_key in fn_dict.keys():
+            if( fn_key not in image_dict[f.TriggerTime][f.SliceLocation].keys()):
+                    image_dict[f.TriggerTime][f.SliceLocation][fn_key] = {}
+            #print(fn_key, filen, fn_dict[fn_key])
+            if (fnmatch.fnmatch(filen, fn_dict[fn_key])):
+                #print('did i get here')
+                if (f.SOPInstanceUID not in image_dict[f.TriggerTime][f.SliceLocation][fn_key].keys()):
+                    image_dict[f.TriggerTime][f.SliceLocation][fn_key][f.SOPInstanceUID] = [filePath]
+                    #print(image_dict[fn_key])
+        if (f.TriggerTime not in trigger_time):
+            trigger_time.append(f.TriggerTime)
+        if (f.SliceLocation not in slice_location):
+            slice_location.append(f.SliceLocation)
+
+#print(slice_location, trigger_time)
+print(sorted(image_dict[image_dict.keys()[0]].keys()))
+"""
+for time in sorted(trigger_time):
+    new_dict[time] = {}
+    for loc in sorted(slice_location):
+        new_dict[time][loc] = {}
+        for var in fn_dict.keys():
+            new_dict[time][loc][var] = {}
+            for image in image_dict.keys():
+                new_dict[time][loc][var][image] = {}
+                if (image_dict[image][0] == var and
+                    image_dict[image][1] == time and 
+                    image_dict[image][2] == loc):
+                    new_dict[time][loc][var][image] = image_dict[image] 
+
+print(new_dict)
+
+for key in dict_test.keys():
+    print(key, dict_test[key])
+print(count)
+"""
